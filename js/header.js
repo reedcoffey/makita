ffe1868d2fd2103aecfba282eceafdee0373e96e
Reed <coffeyreed@proton.me>
2025-10-15 14:34:32 -0700

    Fixed links to index

--- js/header.js
+++ js/header.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
       </a>
       <div class="nav__menu">
         <ul>
-          <li class="nav__menu__link"><a href="#about">About</a></li>
-          <li class="nav__menu__link"><a href="#research">Research</a></li>
-          <li class="nav__menu__link"><a href="#publications">Publications</a></li>
+          <li class="nav__menu__link"><a href="index.html#about">About</a></li>
+          <li class="nav__menu__link"><a href="index.html#research">Research</a></li>
+          <li class="nav__menu__link"><a href="index.html#publications">Publications</a></li>
           <li class="nav__menu__link"><a href="media.html">Media</a></li>
           <li class="nav__menu__link"><a href="cv.html">CV</a></li>
         </ul>
